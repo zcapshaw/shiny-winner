759487d0cf117cbbe37f46d084d01c3edf3bbddd
Zach Capshaw <zcapshaw@gmail.com>
2019-12-15 18:10:37 -0700

    Display todos on the home screen

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Todo = props => (
+    <tr>
+        <td>{props.todo.todoDescription}</td>
+        <td>{props.todo.todoResponsible}</td>
+        <td>{props.todo.todoPriority}</td>
+        <td>
+            <Link to={"/edit/"+props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+)
 
 export default class TodosList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {todos: []};
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/todos')
+            .then(response => {
+                this.setState({todos: response.data})
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    todoList() {
+        return this.state.todos.map(function(currentTodo, index) {
+            return <Todo todo={currentTodo} key={index} />;
+        });
+    }
+
     render() {
         return (
             <div>
-                <p>Welcome to Todos List Component</p>
+                <h3>Todos List</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }}>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Responsible</th>
+                            <th>Priority</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.todoList() }
+                    </tbody>
+                </table>
             </div>
         )
     }
